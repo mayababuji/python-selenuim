4193ac601991e6b0c7a6fab6413ce0005ed57519
mayababuji <mayababuji@outlook.com>
2019-08-30 08:48:31 -0700

    search a keyword pycon in python.org
    Program to search a keyword pycon in http://www.python.org and retrieve the elements in the search result

--- python_org_search_key.py
+++ python_org_search_key.py
@@ -1,7 +1,9 @@
-# This sample code and the prerequiremnts are from https://selenium-python.readthedocs.io/getting-started.html
+#Program to search a keyword pycon in http://www.python.org and retrieve the elements in the search result
+#This sample code and the prerequiremnts are from https://selenium-python.readthedocs.io/getting-started.html
 #Pre-requirement steps
 # 1. pip install selenium
 # 2. find the version of your chrome and download the driver for chrome(eg chromedriver_win32.zip)
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome("C:\\Users\\mbabuji\\Documents\\selenium_chrome_driver\\chromedriver.exe")
@@ -12,3 +14,6 @@ elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
+pycon_results = driver.find_elements_by_css_selector(".list-recent-events")
+print(pycon_results[0].text)
+driver.close()
